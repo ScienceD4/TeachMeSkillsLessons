8ad0242d7cb2bfc884019b19eae4126a2286a152
Dimas <dima991999@mail.ru>
2023-04-02 13:37:28 +0300

    Домашняя работа, задание 1

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HomeWork2;
+
+public class Program
+{
+    public static void Main()
+    {
+        Task1();
+    }
+
+    public static void Task1()
+    {
+        Console.Write("Enter operand1: ");
+        var operand1 = Convert.ToDouble(Console.ReadLine()?.Replace(",","."));
+        Console.Write("Enter operand2: ");
+        var operand2 = Convert.ToDouble(Console.ReadLine()?.Replace(",", "."));
+        Console.Write("Enter the sign of operation: ");
+        var sing = Console.ReadLine();
+        double result;
+
+        switch (sing)
+        {
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "/":
+                if (operand2 == 0)
+                {
+                    Console.WriteLine($"{operand1} {sing} {operand2} = Infinity");
+                    return;
+                }
+                result = operand1 / operand2;
+                break;
+            case "%":
+                result = operand1 % operand2;
+                break;
+            case "^":
+                result = Math.Pow(operand1,operand2);
+                break;
+            default:
+                Console.WriteLine("Operation sign not defined");
+                return;
+        }
+        Console.WriteLine($"{operand1} {sing} {operand2} = {result}");
+    }
+}
