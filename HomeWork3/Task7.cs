f665609858ff714754962107c3cb2e57720e4c81
Dimas <dima991999@mail.ru>
2023-04-06 17:37:14 +0300

    Исправление Task7

--- HomeWork3/Task7.cs
+++ HomeWork3/Task7.cs
@@ -15,11 +15,14 @@ public static class Task7
 
         Console.WriteLine("Array:\n\r");
         Common.Print2DArray(array);
-
         Console.WriteLine();
-        Console.WriteLine($"Max element: {array.MaxElement()}");
-        Console.WriteLine($"Min element: {array.MinElement()}");
-        Console.WriteLine($"Avg: {array.Avg()}");
+
+        var sum = 0;
+        foreach (var num in array)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum: {sum}");
 
         Console.WriteLine(new string('*', 17));
         Console.WriteLine();
