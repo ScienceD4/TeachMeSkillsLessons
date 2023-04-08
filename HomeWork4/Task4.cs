70ac40e59cd7c63457acc8d85c3e6070c10413ca
Dimas <dima991999@mail.ru>
2023-04-08 16:53:38 +0300

    Add homeWor4

---
+++ HomeWork4/Task4.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork4;
+
+/// <summary>
+/// 4. Дана строка: Good day.
+/// Необходимо с помощью метода substring удалить слово "Good". После чего необходимо используя команду insert создать строку со значением: The best day!!!!!!!!!.
+/// Заменить последний "!" на "?" и вывести результат на консоль.
+/// </summary>
+public class Task4 : TaskBase
+{
+    protected override void DoWork()
+    {
+        var text = "Good day";
+
+        text = text.Substring(text.IndexOf(' ') + 1);
+        text = text.Insert(0, "The best ");
+        text = text.Insert(text.Length, "!!!!!!!!!");
+        text = text.Remove(text.Length - 1) + "?";
+
+        Console.WriteLine(text);
+    }
+}
---
+++ HomeWork4/Task6.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeWork4;
+
+/// <summary>
+/// 6. Задайте 2 предложения из консоли.
+/// Для каждого слова первого предложения определите количество его вхождений во второе предложение.
+/// </summary>
+public class Task6 : TaskBase
+{
+    protected override void DoWork()
+    {
+        Console.Write("Enter a stringOne: ");
+        var strOne = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Enter a stringTwo: ");
+        var strTwo = Console.ReadLine();
+        Console.WriteLine();
+
+        var array = strOne.Split();
+
+        var count = 0;
+        foreach (var word in array)
+        {
+            if (strTwo.Contains(word, StringComparison.OrdinalIgnoreCase)) count++;
+        }
+
+        Console.WriteLine($"The strTwo contains {count} words from the strOne");
+    }
+}
