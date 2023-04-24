91526047012368b1f39cf8731352d6190b2c5010
Dimas <dima991999@mail.ru>
2023-04-24 17:58:54 +0300

    print "Ration before"

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -6,8 +6,12 @@ internal class Program
     {
         var ration = RationCreator.Create();
 
+        Console.WriteLine("Ration before");
+        ration.PrintInfo();
+
         var person = new Person("Alex", 1500, ration);
 
+        Console.WriteLine("Person's Ration");
         person.Ration?.PrintInfo();
     }
 }
