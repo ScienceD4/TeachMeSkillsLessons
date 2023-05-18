8ab87320ec76d6597deaa9fff882dee36b58e680
Dimas <dima991999@mail.ru>
2023-05-18 16:00:14 +0300

    Add HomeWork10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork10;
+
+internal class Program
+{
+    static void Main()
+    {
+        var monitor = new PriceMonitor(PrintPrice);
+        var subscriber = new PriceSubscriber();
+        monitor.Subscribe(subscriber);
+
+        for (int i = 0; i < 10; i++)
+        {
+            monitor.GeneratePrice();
+            Console.WriteLine();
+        }
+    }
+
+
+    public static void PrintPrice(decimal price)
+    {
+        Console.WriteLine($"Price from delegate {price:### ###.## RUB}");
+    }
+}
