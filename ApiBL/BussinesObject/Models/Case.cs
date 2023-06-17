1d61ae3112d5d2ab15999bf7dbbbb7f6ad7f83b5
Dimas <dima991999@mail.ru>
2023-06-18 01:20:01 +0300

    Added CaseService and CaseRequest model

---
+++ ApiBL/BussinesObject/Models/Case.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiBL.BussinesObject.Models;
+
+public class CreateCaseRequest
+{
+    [JsonProperty("attachments", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public List<string>? Attachments { get; set; }
+
+    [JsonProperty("tags", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public List<string>? Tags { get; set; }
+
+    [JsonProperty("title")]
+    public string? Title { get; set; }
+
+    [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? Description { get; set; }
+
+    [JsonProperty("preconditions", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? Preconditions { get; set; }
+
+    [JsonProperty("postconditions", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? Postconditions { get; set; }
+
+    [JsonProperty("severity", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public int? Severity { get; set; }
+
+    [JsonProperty("priority", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public int? Priority { get; set; }
+
+    [JsonProperty("automation", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public int? Automation { get; set; }
+
+    [JsonProperty("status", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public int? Status { get; set; }
+
+    [JsonProperty("created_at", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public DateTime? CreatedAt { get; set; }
+
+    [JsonProperty("updated_at", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public DateTime? UpdatedAt { get; set; }
+}
+
+public class CreateCaseBulkRequest
+{
+    [JsonProperty("cases")]
+    public List<CreateCaseRequest>? Cases { get; set; }
+}
---
+++ ApiBL/BussinesObject/Services/CaseService.cs
@@ -0,0 +1,62 @@
+﻿using ApiBL.BussinesObject.Clients;
+using ApiBL.BussinesObject.Models;
+using ApiTests.BussinesObject.Models;
+using ApiTests.Core;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ApiTests.BussinesObject.Services;
+
+public class CaseService : BaseService
+{
+    private string CasesByCodeEndpoint = "case/{code}";
+    private string CasesBulkEndpoint = "case/{code}/bulk";
+    private string CaseByIdEndpoint = "case/{code}/{id}";
+
+    public CaseService(bool withAuth = true) : base(withAuth, new QaseAppClient())
+    {
+    }
+
+    public RestResponse GetAllCasesByProjectCode(string code)
+    {
+        var request = new RestRequest(CasesByCodeEndpoint, Method.Get).AddUrlSegment("code", code);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse GetCaseById(int id, string projectCode)
+    {
+        var request = new RestRequest(CaseByIdEndpoint, Method.Get)
+            .AddUrlSegment("code", projectCode)
+            .AddUrlSegment("id", id);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse CreateCase(CreateCaseRequest caseRequest, string projectCode)
+    {
+        var request = new RestRequest(CasesByCodeEndpoint, Method.Post).AddUrlSegment("code", projectCode);
+        var body = JsonConvert.SerializeObject(caseRequest);
+        request.AddBody(body);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse CreateCaseBulk(CreateCaseBulkRequest caseBulkRequest, string projectCode)
+    {
+        var request = new RestRequest(CasesBulkEndpoint, Method.Post).AddUrlSegment("code", projectCode);
+        var body = JsonConvert.SerializeObject(caseBulkRequest);
+        request.AddBody(body);
+
+        return ApiClient.Execute(request);
+    }
+
+    public RestResponse DeleteCaseById(int id, string projectCode)
+    {
+        var request = new RestRequest(CaseByIdEndpoint, Method.Delete)
+            .AddUrlSegment("code", projectCode)
+            .AddUrlSegment("id", id);
+
+        return ApiClient.Execute(request);
+    }
+}
