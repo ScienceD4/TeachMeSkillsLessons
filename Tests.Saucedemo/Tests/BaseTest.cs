0febece6f9d814e32530720c81033fb99b9e9d05
Dimas <dima991999@mail.ru>
2023-07-01 12:49:03 +0300

    clear BaseTest

--- Tests.Saucedemo/Tests/BaseTest.cs
+++ Tests.Saucedemo/Tests/BaseTest.cs
@@ -10,29 +10,16 @@ namespace Saucedemo.Tests;
 [Parallelizable(ParallelScope.All)]
 public class BaseTest
 {
-    protected AllureLifecycle allure;
-
-    [SetUp]
-    public virtual void SetUp()
-    {
-        allure = AllureLifecycle.Instance;
-    }
-
     [TearDown]
     public virtual void TearDown()
     {
-        //var screen = Browser.Instance.Driver.TakeScreenshot();
-        //var screenBytes = screen.AsByteArray;
-
         if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
         {
             Browser.Instance.TakeScreenShot("ScreenShotFail");
-            //allure.AddAttachment("ScreenShotFail", "image/png", screenBytes);
         }
         else
         {
             Browser.Instance.TakeScreenShot("EndScreenShot");
-            //allure.AddAttachment("ScreenShot", "image/png", screenBytes);
         }
 
         Browser.Instance.CloseBrowser();
